6807fe073bf2db66425e57addfe8ef3afb066dec
steffkes <stefan@gelungen.es>
2024-07-04 17:13:39 +0200

    implement simple image with canvas

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:22.4.0-alpine3.19
+
+RUN apk add build-base g++ cairo-dev pango-dev giflib-dev
--- api/index.js
+++ api/index.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const { createCanvas, loadImage } = require("canvas");
 
-app.get('/', function (req, res) {
-  res.send({ api: true, hello: "world" })
-});
+const app = express()
+  .get("/", function (req, res) {
+    res.send({ api: true, hello: "world" });
+  })
+  .get("/image", function (req, res) {
+    const colors = ["red", "yellow", "green"];
+    const [width, height] = [10, 10];
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+    const canvas = createCanvas(width * colors.length, height);
+    const ctx = canvas.getContext("2d");
+
+    for (const [index, color] of colors.entries()) {
+      ctx.fillStyle = color;
+      ctx.fillRect(width * index, 0, width, height);
+    }
+
+    res.type("png").send(canvas.toBuffer());
+  })
+  .listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
